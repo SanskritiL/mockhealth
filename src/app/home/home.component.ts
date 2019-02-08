666af39ca4df468c252264071b4ff6432c4c22ae
Sanskriti Lamsal <sanskritilamsal@hst000147860um.wifi02.olemiss.edu>
2019-02-07 21:33:09 -0600

    nav bar made

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,14 @@ import { Observable } from 'rxjs';
 import { AngularFireStorage } from "@angular/fire/storage";
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import { IfStmt } from '@angular/compiler';
+import * as $ from 'jquery';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   profileUrl: Observable<string | null>;
@@ -38,7 +40,88 @@ export class HomeComponent implements OnInit {
   
  displayHome(){
 
+  //for css
+  const menuBtn = document.querySelector('.menu-btn');
+  const menu = document.querySelector('.menu');
+  const menuNav = document.querySelector('.menu-nav');
+  const navItems = document.querySelectorAll('.nav-items');
+  //set initial state of menu
+  let showMenu = false;
+  menuBtn.addEventListener('click',toggleMenu);
+  function toggleMenu(){
+    if(!showMenu){
+        menuBtn.classList.add('close');
+        
+      
+
+        showMenu= true;
+    }
+    else{
+      menuBtn.classList.remove('close');
+     
+
+
+
+      showMenu = false;
+    }
+  }
+
+/*
+  let sliderImages = document.querySelectorAll('.slide'),
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector('#arrow-right'),
+  current = 0;
+  console.log(sliderImages)
+//clear all images
+function reset(){
+ for(let i =0; i < sliderImages.length;i++){
+   sliderImages[i].style.display = 'none';
  }
+}
+//init slider
+function startSlide(){
+ reset();
+ 
+ sliderImages[0].style.display='block';
+ 
+}
+//Show prev
+function slideLeft(){
+ reset();
+ sliderImages[current-1].style.display='block';
+ current--;
+}
+
+//show next 
+function slideRight(){
+ reset();
+ sliderImages[current+1].style.display='block';
+ current++;
+}
+//left arrow clik
+arrowLeft.addEventListener('click',function(){
+ console.log("meow")
+ if(current === 0){
+   current = sliderImages.length;
+ }
+ slideLeft();
+});
+
+//right arrow clik
+arrowRight.addEventListener('click',function(){
+ if(current === sliderImages.length-1){
+   current = -1
+ }
+ slideRight();
+});
+
+
+startSlide();
+
+*/
+
+ }
+ 
 
 
   /*ngOnInit() {
@@ -70,7 +153,22 @@ export class HomeComponent implements OnInit {
 
  
 */
+
+
+
+
+
+
 ngOnInit(){
+
+     
+this.displayHome()
+
+
+
+      
+
+
   this.service.getUserDetails().subscribe(actionArray=>{
    actionArray.map(item => {
 
@@ -86,12 +184,21 @@ ngOnInit(){
 
      }
      else{
-       this.fullname ="You're being redirected. <3 <3"
-     }
+       console.log("You're being redirected. <3 <3"
+            )     }
 
     })
   })
 
 }
+
+//select DOM Items
+ 
+
+
+
+
+
+
 
 }
